7c4d3bbdebe75ae53e849d262c318bbc32a4dc16
Nate-Soul <nanithsukpong@gmail.com>
2024-07-02 14:35:27 +0100

    initial reserve booking website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        title: "Home Page",
+        component: HomeComponent
+    },
+    {
+        path: "details/:id",
+        title: "Home Details",
+        component: DetailsComponent
+    },
+];
